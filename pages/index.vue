07937f876862872a5f35f98336d1a2fe2ed900a9
KAIMonmoy <aftarulislam@gmail.com>
2020-12-12 17:42:28 +0600

    :tada: Initializing Repo

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h1>1605063</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  head() {
+    return {
+      title: "HMM Offline - Ghost Finder",
+    };
+  },
+};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
